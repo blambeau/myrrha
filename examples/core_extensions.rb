d3c586775a4b6c28802ef6ca133675e482434f49
Bernard Lambeau <blambeau@gmail.com>
2011-07-20 17:56:31 +0200

    Same renaming in examples

--- examples/core_extensions.rb
+++ examples/core_extensions.rb
@@ -2,22 +2,22 @@ require File.expand_path('../examples_helper', __FILE__)
 require 'myrrha/core_ext'
 
 # it works on numerics
-coerce("12", Integer).should      eq(12)
-coerce("12.0", Float).should      eq(12.0)
+coercion("12", Integer).should eq(12)
+coercion("12.0", Float).should eq(12.0)
 
 # but also on regexp (through Regexp.compile)
-coerce("[a-z]+", Regexp).should   eq(/[a-z]+/)
+coercion("[a-z]+", Regexp).should eq(/[a-z]+/)
 
 # and, yes, on Boolean (Sorry Matz!)
-coerce("true", Boolean).should    eq(true)      
-coerce("false", Boolean).should   eq(false)
+coercion("true", Boolean).should eq(true)      
+coercion("false", Boolean).should eq(false)
 
 # and on date and time (through Date/Time.parse)  
 require 'date'
 require 'time'
-coerce("2011-07-20", Date).should       be_a(Date)  
-coerce("2011-07-20 10:57", Time).should be_a(Time)
+coercion("2011-07-20", Date).should be_a(Date)  
+coercion("2011-07-20 10:57", Time).should be_a(Time)
 
 # why not on URI?
 require 'uri'
-coerce('http://google.com', URI).should be_a(URI)    
+coercion('http://google.com', URI).should be_a(URI)    
