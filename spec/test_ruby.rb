e7f8c98926071743f1239497284fdd1c952d9a87
Bernard Lambeau <blambeau@gmail.com>
2011-07-20 10:52:55 +0200

    Cleaned test_ruby spec

--- spec/test_ruby.rb
+++ spec/test_ruby.rb
@@ -1,54 +1,69 @@
 require 'spec_helper'
-describe "Coercer::Ruby" do
+describe "Coercer::Ruby's coercion " do
   
   let(:graph){ Coercer::Ruby }
     
-  specify "Integer" do
-    # from Integer
-    graph.coerce(12,     Integer).should eq(12)
-    # from String
-    graph.coerce("12",   Integer).should eq(12)
-    graph.coerce("-12",  Integer).should eq(-12)
-    graph.coerce("0",    Integer).should eq(0)
-    # on error
-    lambda{graph.coerce("abc",  Integer)}.should raise_error(Coercer::Error)
+  describe "to Integer" do
+    specify "from Integer" do
+      graph.coerce(12,     Integer).should eq(12)
+    end
+    specify "from String" do
+      graph.coerce("12",   Integer).should eq(12)
+      graph.coerce("-12",  Integer).should eq(-12)
+      graph.coerce("0",    Integer).should eq(0)
+    end
+    specify "on error" do
+      lambda{graph.coerce("abc",  Integer)}.should raise_error(Coercer::Error)
+    end
   end
   
-  specify "Float" do
-    # from Float
-    graph.coerce(12.2,   Float).should eq(12.2)
-    # from Integer
-    graph.coerce(0,      Float).should eq(0.0)
-    graph.coerce(12,     Float).should eq(12.0)
-    graph.coerce(-12,    Float).should eq(-12.0)
-    # from String
-    graph.coerce("12.2", Float).should eq(12.2)
-    graph.coerce("0",    Float).should eq(0.0)
-    graph.coerce("0.0",  Float).should eq(0.0)
-    graph.coerce("-12.2", Float).should eq(-12.2)
-    # on error
-    lambda{graph.coerce("abc",  Float)}.should raise_error(Coercer::Error)
+  describe "to Float" do
+    specify "from Float" do
+      graph.coerce(12.2,   Float).should eq(12.2)
+    end
+    specify "from Integer" do
+      graph.coerce(0,      Float).should eq(0.0)
+      graph.coerce(12,     Float).should eq(12.0)
+      graph.coerce(-12,    Float).should eq(-12.0)
+    end
+    specify "from String" do
+      graph.coerce("12.2", Float).should eq(12.2)
+      graph.coerce("0",    Float).should eq(0.0)
+      graph.coerce("0.0",  Float).should eq(0.0)
+      graph.coerce("-12.2", Float).should eq(-12.2)
+    end
+    specify "on error" do
+      lambda{graph.coerce("abc",  Float)}.should raise_error(Coercer::Error)
+    end
   end
   
-  specify "Numeric" do
-    # from String
-    graph.coerce("12",   Numeric).should eq(12)
-    graph.coerce("12.2", Numeric).should eq(12.2)
+  describe "to Numeric" do
+    specify "from String" do
+      graph.coerce("12",   Numeric).should eq(12)
+      graph.coerce("12.2", Numeric).should eq(12.2)
+    end
   end
   
-  specify "Boolean" do    
-    # from Boolean
-    graph.coerce(true,  Boolean).should eq(true)
-    graph.coerce(false, Boolean).should eq(false)
-    # from String
-    graph.coerce("true",  Boolean).should eq(true)
-    graph.coerce("false", Boolean).should eq(false)
-    # on Error
-    lambda{graph.coerce("abc", Boolean)}.should raise_error(Coercer::Error)
+  describe "to Boolean" do    
+    specify "from Boolean" do
+      graph.coerce(true,  Boolean).should eq(true)
+      graph.coerce(false, Boolean).should eq(false)
+    end
+    specify "from String" do
+      graph.coerce("true",  Boolean).should eq(true)
+      graph.coerce("false", Boolean).should eq(false)
+    end
+    specify "on error" do
+      lambda{graph.coerce("abc", Boolean)}.should raise_error(Coercer::Error)
+    end
   end
   
-  specify "Date" do
-    graph.coerce("2011/07/20", Date).should eq(Date.parse("2011-07-20"))
+  describe "to Date" do
+    let(:expected){ Date.parse("2011-07-20") }
+    specify "from String" do
+      graph.coerce("2011/07/20", Date).should eq(expected)
+      graph.coerce("2011/07/20", Date).should eq(expected)
+    end
   end
       
 end
