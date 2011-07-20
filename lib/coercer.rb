7860833d23f403d80a18db2abef351bd58b923c5
Bernard Lambeau <blambeau@gmail.com>
2011-07-20 10:07:50 +0200

    Much simpler this way

--- lib/coercer.rb
+++ lib/coercer.rb
@@ -10,39 +10,13 @@ module Coercer
   #
   class Graph
     
-    class Node
-      attr_accessor :domain
-      def initialize(domain)
-        @domain = domain
-      end
-      def out_edges
-        @out_edges || []
-      end
-      def add_out_edge(edge)
-        @out_edges ||= []
-        @out_edges << edge
-      end
-    end
-    
-    class Edge
-      attr_reader :source
-      attr_reader :target
-      attr_reader :converter
-      def initialize(source, target, converter)
-        @source = source
-        @target = target
-        @converter = converter
-      end
-    end
-    
     def initialize
-      @nodes = []
+      @edges = []
       yield(self) if block_given?
     end
     
     def coercion(source, target, converter = nil, &convproc)
-      converter ||= convproc
-      connect(node(source, true), node(target, true), converter)
+      @edges << [source, target, converter || convproc] 
     end
     
     def coerce(value, target_domain)
@@ -50,20 +24,13 @@ module Coercer
       return value if belongs_to?(value, target_domain)
       
       error = nil
-      # 2) find the corresponding source domain
-      @nodes.each do |source_node|
-        next unless belongs_to?(value, source_node.domain)
-        
-        # 3) look at the edges
-        source_node.out_edges.each do |edge|
-          next unless subdomain?(edge.target.domain, target_domain)
-
-          # 4) apply coercion
-          begin
-            return edge.converter.call(value)
-          rescue => ex
-            error = ex.message unless error
-          end
+      @edges.each do |from,to,converter|
+        next unless belongs_to?(value, from)
+        next unless subdomain?(to, target_domain)
+        begin
+          return converter.call(value)
+        rescue => ex
+          error = ex.message unless error
         end
       end
       msg = "Unable to coerce `#{value}` to #{target_domain}"
@@ -80,22 +47,6 @@ module Coercer
       child.superclass ? 
         subdomain?(child.superclass, parent) :
         false
-    end
-    
-    protected
-    
-    def node(domain, create)
-      found = @nodes.find{|n| n.domain == domain}
-      if found.nil? && create
-        found = Node.new(domain)
-        @nodes << found
-      end
-      found
-    end
-    
-    def connect(source, target, converter)
-      edge = Edge.new(source, target, converter)
-      source.add_out_edge(edge)
     end
     
   end # class Graph
