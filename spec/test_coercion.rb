93eed45bebd33af51c9ccc981f08c20d2132ecfc
Bernard Lambeau <blambeau@gmail.com>
2011-07-22 09:44:53 +0200

    We should be stronger here

--- spec/test_coercion.rb
+++ spec/test_coercion.rb
@@ -10,12 +10,12 @@ describe "::Ruby's coercion " do
   
   describe "to Integer" do
     specify "from Integer" do
-      coercion(12,     Integer).should eq(12)
+      coercion(12,     Integer).should eql(12)
     end
     specify "from String" do
-      coercion("12",   Integer).should eq(12)
-      coercion("-12",  Integer).should eq(-12)
-      coercion("0",    Integer).should eq(0)
+      coercion("12",   Integer).should eql(12)
+      coercion("-12",  Integer).should eql(-12)
+      coercion("0",    Integer).should eql(0)
     end
     specify "on error" do
       lambda{coercion("abc",  Integer)}.should raise_error(Myrrha::Error)
@@ -24,18 +24,18 @@ describe "::Ruby's coercion " do
   
   describe "to Float" do
     specify "from Float" do
-      coercion(12.2,   Float).should eq(12.2)
+      coercion(12.2,   Float).should eql(12.2)
     end
     specify "from Integer" do
-      coercion(0,      Float).should eq(0.0)
-      coercion(12,     Float).should eq(12.0)
-      coercion(-12,    Float).should eq(-12.0)
+      coercion(0,      Float).should eql(0.0)
+      coercion(12,     Float).should eql(12.0)
+      coercion(-12,    Float).should eql(-12.0)
     end
     specify "from String" do
-      coercion("12.2", Float).should eq(12.2)
-      coercion("0",    Float).should eq(0.0)
-      coercion("0.0",  Float).should eq(0.0)
-      coercion("-12.2", Float).should eq(-12.2)
+      coercion("12.2", Float).should eql(12.2)
+      coercion("0",    Float).should eql(0.0)
+      coercion("0.0",  Float).should eql(0.0)
+      coercion("-12.2", Float).should eql(-12.2)
     end
     specify "on error" do
       lambda{coercion("abc",  Float)}.should raise_error(Myrrha::Error)
@@ -44,19 +44,19 @@ describe "::Ruby's coercion " do
   
   describe "to Numeric" do
     specify "from String" do
-      coercion("12",   Numeric).should eq(12)
-      coercion("12.2", Numeric).should eq(12.2)
+      coercion("12",   Numeric).should eql(12)
+      coercion("12.2", Numeric).should eql(12.2)
     end
   end
   
   describe "to Boolean" do    
     specify "from Boolean" do
-      coercion(true,  Boolean).should eq(true)
-      coercion(false, Boolean).should eq(false)
+      coercion(true,  Boolean).should eql(true)
+      coercion(false, Boolean).should eql(false)
     end
     specify "from String" do
-      coercion("true",  Boolean).should eq(true)
-      coercion("false", Boolean).should eq(false)
+      coercion("true",  Boolean).should eql(true)
+      coercion("false", Boolean).should eql(false)
     end
     specify "on error" do
       lambda{coercion("abc", Boolean)}.should raise_error(Myrrha::Error)
@@ -66,40 +66,40 @@ describe "::Ruby's coercion " do
   describe "to Date" do
     let(:expected){ Date.parse("2011-07-20") }
     specify "from String" do
-      coercion("2011-07-20", Date).should eq(expected)
-      coercion("2011/07/20", Date).should eq(expected)
+      coercion("2011-07-20", Date).should eql(expected)
+      coercion("2011/07/20", Date).should eql(expected)
     end
   end
       
   describe "to Time" do
     let(:expected){ Time.parse("2011-07-20 10:53") }
     specify "from String" do
-      coercion("2011-07-20 10:53", Time).should eq(expected)
+      coercion("2011-07-20 10:53", Time).should eql(expected)
     end
   end
   
   describe "to Symbol" do
     specify "from Symbol" do
-      coercion(:hello, Symbol).should eq(:hello)
+      coercion(:hello, Symbol).should eql(:hello)
     end
     specify "from String" do
-      coercion("hello", Symbol).should eq(:hello)
+      coercion("hello", Symbol).should eql(:hello)
     end
   end
   
   describe "to Regexp" do
     specify "from Regexp" do
-      coercion(/[a-z]+/, Regexp).should eq(/[a-z]+/)
+      coercion(/[a-z]+/, Regexp).should eql(/[a-z]+/)
     end
     specify "from String" do
-      coercion("[a-z]+", Regexp).should eq(/[a-z]+/)
+      coercion("[a-z]+", Regexp).should eql(/[a-z]+/)
     end
   end
   
   describe "to URI" do
     require 'uri'
     specify "from String" do
-      coercion("http://www.google.com/", URI).should eq(URI.parse("http://www.google.com/"))
+      coercion("http://www.google.com/", URI).should eql(URI.parse("http://www.google.com/"))
     end
   end
 
