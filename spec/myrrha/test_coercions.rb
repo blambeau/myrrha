eb7fca9c7450c9badcf9f81c597370e602809026
Bernard Lambeau <blambeau@gmail.com>
2011-07-23 23:05:25 +0200

    Check the other way around

--- spec/myrrha/test_coercions.rb
+++ spec/myrrha/test_coercions.rb
@@ -5,8 +5,10 @@ describe "Myrrha.coercions" do
     name = Myrrha.domain(:Name){|s| s.is_a?(Symbol)}
     rules = Myrrha.coercions do |r|
       r.coercion String, name, lambda{|s,t| s.to_sym}
+      r.coercion name, String, lambda{|s,t| s.to_s}
     end
     rules.coerce("hello", name).should eq(:hello)
+    rules.coerce(:hello, String).should eq("hello")
   end
   
 end
