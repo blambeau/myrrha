6aca0b8c2f5401013bc5b3f40f71c6e8e56a7df5
Bernard Lambeau <blambeau@gmail.com>
2011-07-24 11:07:11 +0200

    Refactored test

--- spec/myrrha/test_domain.rb
+++ spec/myrrha/test_domain.rb
@@ -2,59 +2,55 @@ require 'spec_helper'
 module Myrrha
   describe "#domain" do
     
-    describe "with only a predicate" do
-      subject{ Myrrha.domain{|s| s == 12} }
-      specify{ 
-        subject.should be_a(Class) 
-        subject.superclass.should eq(Object)
-        (subject === 12).should be_true
-        (subject === 13).should be_false
-      }
+    specify "the basic contract" do
+      subject = Myrrha.domain{|s| s == 12}
+      subject.should be_a(Class) 
+      subject.superclass.should eq(Object)
+      (subject === 12).should be_true
+      (subject === 13).should be_false
     end
     
-    describe "with a super domain and a predicate" do
-      subject{ Myrrha.domain(Integer){|i| i > 0} }
-      specify{ 
-        subject.should be_a(Class) 
-        subject.superclass.should eq(Integer)
-        (subject === 12).should be_true
-        (subject === 0).should be_false
-      }
+    specify "with a ruby superclass" do
+      subject = Myrrha.domain(Integer){|i| i > 0}
+      subject.should be_a(Class) 
+      subject.superclass.should eq(Integer)
+      (subject === 12).should be_true
+      (subject === 0).should be_false
     end
     
-    specify "when affected to a constant" do
+    describe "A factored sub domain of Integer" do
       PosInt = Myrrha.domain(Integer){|i| i > 0}
-      PosInt.name.should eq("Myrrha::PosInt")
-    end
-    
-    describe ".new" do
-
-      specify "when call to super is not required" do
-        posint = Myrrha.domain(Integer){|i| i > 0}
-        posint.new(12).should eq(12)
+      specify("#name") {
+        PosInt.name.should eq("Myrrha::PosInt")
+      }
+      specify("#new") {
+        PosInt.new(12).should eq(12)
         lambda {
-          posint.new(0)
+          PosInt.new(0)
         }.should raise_error(ArgumentError)
-      end
+      }
+    end
       
-      specify "when call to super is required" do
-        class Color
-          attr_reader :r
-          attr_reader :g
-          attr_reader :b
-          def initialize(r,g,b)
-            @r, @g, @b = r, g, b
-          end
+    describe "A factored sub domain of a user-defined Class" do
+      class Color
+        attr_reader :r
+        attr_reader :g
+        attr_reader :b
+        def initialize(r,g,b)
+          @r, @g, @b = r, g, b
         end
-        RedToZero = Myrrha.domain(Color){|c| c.r == 0}
+      end
+      RedToZero = Myrrha.domain(Color){|c| c.r == 0}
+      specify("#===") {
         (RedToZero === Color.new(0,1,1)).should be_true
         (RedToZero === Color.new(1,1,1)).should be_false
+      }
+      specify("#new") {
         RedToZero.new(0, 1, 1).should be_a(Color)
         lambda{
           RedToZero.new(1, 1, 1)
         }.should raise_error(ArgumentError)
-      end
-      
+      }
     end
     
   end
