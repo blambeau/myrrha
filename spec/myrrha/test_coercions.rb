35893fd0997578b524ccd6abc42b02b3b3c3878a
Bernard Lambeau <blambeau@gmail.com>
2011-07-24 10:28:31 +0200

    Check that coercions work

---
+++ spec/myrrha/test_coercions.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+describe "Myrrha.coercions" do
+  
+  it "should support using user-defined domains" do
+    name = Myrrha.domain{|s| s.is_a?(Symbol)}
+    rules = Myrrha.coercions do |r|
+      r.coercion String, name, lambda{|s,t| s.to_sym}
+      r.coercion name, String, lambda{|s,t| s.to_s}
+    end
+    rules.coerce("hello", name).should eq(:hello)
+    rules.coerce(:hello, String).should eq("hello")
+  end
+  
+end
