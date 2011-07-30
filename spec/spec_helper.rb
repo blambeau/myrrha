e0c65421d3005927a293ebc76f1e5be61e8ff3f6
Bernard Lambeau <blambeau@gmail.com>
2011-07-30 13:42:47 +0200

    Still complaining...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,30 +5,28 @@ require 'myrrha/to_ruby_literal'
 require 'date'
 require 'shared/a_value'
 
-unless defined?(Myrrha::SAFE_VALUES)
-  Myrrha::SAFE_VALUES = {
-    NilClass   => [ nil ],
-    TrueClass  => [ true ],
-    FalseClass => [ false ],
-    Fixnum     => [ -(2**(0.size * 8 - 2)), -1, 0, 1, 10, (2**(0.size * 8 - 2) - 1)],
-    Bignum     => [ -(2**(0.size * 8 - 2)) - 1, (2**(0.size * 8 - 2)) ],
-    Float      => [ -0.10, 0.0, 0.10 ],
-    String     => ['', 'hello'],
-    Symbol     => [ :hello, :"s-b-y-c", :"12" ],
-    Class      => [ Integer, ::Struct::Tms ],
-    Module     => [ Kernel, Myrrha ],
-    Regexp     => [ /a-z/, /^$/, /\s*/, /[a-z]{15}/ ],
-    Range      => [ 0..10, 0...10 ],
-    Array      => [ [], [nil], [1, "hello"] ],
-    Hash       => [ {}, {1 => 2, :hello => "world"} ]
-  }
-  Myrrha::UNSAFE_VALUES = {
-    Date  => [ Date.today ],
-    Time  => [ Time.now   ],
-    Array => [ [Date.today, Time.now] ],
-    Hash  => [ {Date.today => Time.now} ],
-    Range => [ Date.today..(Date.today+1) ]
-  }
-  Myrrha::VALUES = Myrrha::SAFE_VALUES.values.inject([], :+) + 
-                   Myrrha::UNSAFE_VALUES.values.inject([], :+)
-end
+$SAFE_VALUES = {
+  NilClass   => [ nil ],
+  TrueClass  => [ true ],
+  FalseClass => [ false ],
+  Fixnum     => [ -(2**(0.size * 8 - 2)), -1, 0, 1, 10, (2**(0.size * 8 - 2) - 1)],
+  Bignum     => [ -(2**(0.size * 8 - 2)) - 1, (2**(0.size * 8 - 2)) ],
+  Float      => [ -0.10, 0.0, 0.10 ],
+  String     => ['', 'hello'],
+  Symbol     => [ :hello, :"s-b-y-c", :"12" ],
+  Class      => [ Integer, ::Struct::Tms ],
+  Module     => [ Kernel, Myrrha ],
+  Regexp     => [ /a-z/, /^$/, /\s*/, /[a-z]{15}/ ],
+  Range      => [ 0..10, 0...10 ],
+  Array      => [ [], [nil], [1, "hello"] ],
+  Hash       => [ {}, {1 => 2, :hello => "world"} ]
+}
+$UNSAFE_VALUES = {
+  Date  => [ Date.today ],
+  Time  => [ Time.now   ],
+  Array => [ [Date.today, Time.now] ],
+  Hash  => [ {Date.today => Time.now} ],
+  Range => [ Date.today..(Date.today+1) ]
+}
+$VALUES = $SAFE_VALUES.values.inject([], :+) + 
+          $UNSAFE_VALUES.values.inject([], :+)
--- spec/test_value.rb
+++ spec/test_value.rb
@@ -1,4 +1,4 @@
-Myrrha::VALUES.each do |value|
+$VALUES.each do |value|
   describe "#{value} (#{value.class})" do
     subject{ value }
     it_should_behave_like "a value" 
