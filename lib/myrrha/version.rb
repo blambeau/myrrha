4d09eef9a942e2d78a63d6cf004c2337046f8f67
Bernard Lambeau <blambeau@gmail.com>
2012-01-26 19:17:30 +0100

    Bumped up all dependencies, including noe.ruby

--- lib/myrrha/version.rb
+++ lib/myrrha/version.rb
@@ -1,14 +1,14 @@
 module Myrrha
   module Version
-  
+
     MAJOR = 1
     MINOR = 2
     TINY  = 1
-  
+
     def self.to_s
       [ MAJOR, MINOR, TINY ].join('.')
     end
-  
-  end 
+
+  end
   VERSION = Version.to_s
 end
