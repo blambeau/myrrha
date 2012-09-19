f9cd96b1b93fdd6344d302f92d0942324ca6fa6f
Bernard Lambeau <blambeau@gmail.com>
2012-09-19 14:39:29 +0200

    Bump version number to 2.0.0

--- lib/myrrha/version.rb
+++ lib/myrrha/version.rb
@@ -1,9 +1,9 @@
 module Myrrha
   module Version
 
-    MAJOR = 1
-    MINOR = 2
-    TINY  = 2
+    MAJOR = 2
+    MINOR = 0
+    TINY  = 0
 
     def self.to_s
       [ MAJOR, MINOR, TINY ].join('.')
