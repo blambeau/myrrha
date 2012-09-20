3cfadb77365e23bcb3d8f5502f68ef0c63a46172
Bernard Lambeau <blambeau@gmail.com>
2012-09-20 15:33:06 +0200

    Strip spacing.

--- lib/myrrha/coerce.rb
+++ lib/myrrha/coerce.rb
@@ -13,10 +13,10 @@ module Myrrha
   #
   # This method mimics Ruby's Integer(), Float(), etc. for Boolean values.
   #
-  # @param [Object] s a Boolean or a String 
+  # @param [Object] s a Boolean or a String
   # @return [Boolean] true if `s` is already true of the string 'true',
   #                   false if `s` is already false of the string 'false'.
-  # @raise [ArgumentError] if `s` cannot be coerced to a boolean. 
+  # @raise [ArgumentError] if `s` cannot be coerced to a boolean.
   #
   def self.Boolean(s)
     if (s==true || s.to_str.strip == "true")
@@ -27,28 +27,28 @@ module Myrrha
       raise ArgumentError, "invalid value for Boolean: \"#{s}\""
     end
   end
-  
-  # Defines basic coercions for Ruby, mostly from String 
+
+  # Defines basic coercions for Ruby, mostly from String
   Coerce = coercions do |g|
-    
+
     # Returns a constant denoted by `s`
     def g.constant_lookup(s, target_domain)
-      found = (s.split('::') - [""]).inject(Kernel){|cur,n| 
+      found = (s.split('::') - [""]).inject(Kernel){|cur,n|
         cur.const_get(n.to_sym)
       }
       belongs_to?(found, target_domain) ? found : throw(:nextrule)
     end
-    
+
     # NilClass should return immediately
-    g.upon(NilClass) do |s,t| 
+    g.upon(NilClass) do |s,t|
       nil
     end
-    
+
     # Use t.coerce if it exists
     g.upon(lambda{|s,t| t.respond_to?(:coerce)}) do |s,t|
       t.coerce(s)
     end
-    
+
     # Specific basic rules
     g.coercion Object,  String, lambda{|s,t| String(s)                     }
     g.coercion String, Integer, lambda{|s,t| Integer(s)                    }
@@ -62,18 +62,18 @@ module Myrrha
     g.coercion String,   Class, lambda{|s,t| g.constant_lookup(s, t)       }
     g.coercion String,  Module, lambda{|s,t| g.constant_lookup(s, t)       }
     g.coercion String,    Time, lambda{|s,t| require 'time'; Time.parse(s) }
-      
-    # By default, we try to invoke :parse on the class 
-    g.fallback(String) do |s,t| 
-      t.respond_to?(:parse) ? t.parse(s.to_str) : throw(:nextrule) 
+
+    # By default, we try to invoke :parse on the class
+    g.fallback(String) do |s,t|
+      t.respond_to?(:parse) ? t.parse(s.to_str) : throw(:nextrule)
     end
-    
+
   end # Coerce
 
   def self.coerce(value, domain)
     Coerce.apply(value, domain)
   end
-    
+
 end # module Myrrha
 
 if Myrrha.core_ext?
@@ -85,6 +85,6 @@ if Myrrha.core_ext?
     private
     def coerce(value, domain)
       Myrrha.coerce(value, domain)
-    end 
+    end
   end
 end
